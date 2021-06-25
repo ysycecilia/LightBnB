83d3b6a59cce5e9c5a263c9b519ffbdfcb06f277
Cecilia <ysycecilia@gmail.com>
2021-06-25 03:26:16 +0000

    refactor addproperty() by using pool.query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -161,7 +161,7 @@ const getAllProperties = function(options, limit = 10) {
     }
   }
 
-  if (options.minimum_rating) {
+  if (options.minimum_rating) { 
     queryParams.push(options.minimum_rating);
     if (queryParams.leng === 1) {
       queryString += `where rating >= $${minimum_rating}`;
@@ -193,9 +193,30 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+  return pool.query(`
+  insert into properties (owner_id, title, description, 
+    thumbnail_photo_url, cover_photo_url, cost_per_night,
+    street, city, province, post_code, country,
+    parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`,
+  [property.owner_id, property.title, property.description,
+    property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country,
+    property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(res => {
+      if (res) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => {
+      return err;
+    });
+    
 };
 exports.addProperty = addProperty;
